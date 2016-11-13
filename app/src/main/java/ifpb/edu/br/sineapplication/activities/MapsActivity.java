bcf7d4a9051b78d4484f2d7e9e576ff928fc25dd
Eddie Kaleb <kaleb.eddie964@gmail.com>
2016-11-13 18:06:50 -0300

    sines cg in maps

--- app/src/main/java/ifpb/edu/br/sineapplication/activities/MapsActivity.java
+++ app/src/main/java/ifpb/edu/br/sineapplication/activities/MapsActivity.java
@@ -2,6 +2,7 @@ package ifpb.edu.br.sineapplication.activities;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,12 +11,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import ifpb.edu.br.sineapplication.R;
+import ifpb.edu.br.sineapplication.objects.Sine;
+import ifpb.edu.br.sineapplication.service.APISine;
+import ifpb.edu.br.sineapplication.service.ConnectionServer;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private List<Sine> sines;
+    private LatLng latLng;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +49,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Referencing the service's interface
+                APISine APISine = ConnectionServer.getInstance().getService();
+                //Calling the request method with two parameters
+                Call<List<Sine>> call = APISine.getSinesWithCoordinates(-7.242662,-35.971605,"100");
+                //Executing de request GET
+                call.enqueue(new Callback<List<Sine>>() {
+                    @Override
+                    public void onResponse(Call<List<Sine>> call, Response<List<Sine>> response) {
+                        if (response.isSuccessful()) {
+                            sines = response.body();
+                            for (int i=0; i<sines.size(); i++) {
+                                latLng = new LatLng(Double.parseDouble(sines.get(i).getLat()), Double.parseDouble(sines.get(i).getLongitude()));
+                                mMap.addMarker(new MarkerOptions().position(latLng).title(sines.get(i).getNome()));
+                            }
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                            Log.i("onResponse", "OK");
+                        } else {
+                            Log.i("onResponse", "ERRO");
+                        }
+                    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    @Override
+                    public void onFailure(Call<List<Sine>> call, Throwable t) {
+                        Log.i("onFailure", t.getMessage().toString());
+                    }
+                });
+            }
+        }).start();
+
     }
 }
